c9e0d169d5644017e805da53f31c65359c336d94
varunyagain <74807414+varunyagain@users.noreply.github.com>
2026-02-17 18:05:38 +0000

    Deploying to gh-pages from @ varunyagain/exult@666efae81167939c340fbf0b88ede1216c6055c3 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2644077473" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "265295870" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
